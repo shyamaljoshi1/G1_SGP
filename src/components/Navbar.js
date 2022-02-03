a3041b558400a103de61deca34ab98f00bc02503
Shyamal Joshi <shyamaljoshi2003@gmail.com>
2022-02-03 12:26:13 +0530

    started navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Nav=styled.nav`
+    height:60px;
+    background:red;
+`;
+
+const Logo = styled(Link)`
+    color:#fff;
+`;
+
+const Menubars=styled.i``;
+const Navmenu=styled.div``;
+
+
+const Navbar = () => {
+return (
+    <Nav>
+        <Logo>LOGO</Logo>
+        <Menubars/>
+        <Navmenu/>
+        <h1>Navbar</h1>
+    </Nav>
+)
+};
+
+export default Navbar;
