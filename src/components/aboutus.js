172b3187f4db461a89d8dc68bbece4fbafc24d5f
padmanabh <padmanabhkhunt@gmail.com>
2022-03-12 22:56:50 +0530

    Merge branch 'main' of https://github.com/shyamaljoshi1/G1_SGP

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import styled from "styled-components";
+import imaggg from "../images/Image-5.jpg";
 
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 80vh;
+  object-fit: cover;
+`;
 const AboutUs=()=> {
   return (
-    <div>aboutus</div>
+    <HeroImage src={imaggg} />
   )
 }
 
