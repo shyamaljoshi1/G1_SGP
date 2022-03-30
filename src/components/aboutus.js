012ee3792269936ad2a3905fb7d22a17f81548a4
padmanabh <padmanabhkhunt@gmail.com>
2022-03-30 11:21:38 +0530

    About page

--- src/components/aboutus.js
+++ src/components/aboutus.js
@@ -9,10 +9,36 @@ const HeroImage = styled.img`
   width: 100vw;
   height: 80vh;
   object-fit: cover;
-`;
-const AboutUs=()=> {
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  box-shadow: 0px 9px 20px rgba(0, 0, 0, 0.5);
+  `;
+
+const HeroAbout = styled.div`
+  position: absolute;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  max-width: 1600px;
+  width: calc(100%-100px);
+  color: #fff;
+  padding: 20px;
+  overflow: hidden;
+  bottom: 13rem;
+  right: 3rem;
+  letter-spacing: 15px;
+  text-shadow: 0px 0px 30px #fff;
+`
+const AboutUs = () => {
   return (
-    <HeroImage src={imaggg} />
+    <>
+      <HeroImage src={imaggg} />
+      <HeroAbout>
+        <h1>ABOUT RESOULT CONSULTANTS</h1>
+      </HeroAbout>
+      
+    </>
+
   )
 }
 
